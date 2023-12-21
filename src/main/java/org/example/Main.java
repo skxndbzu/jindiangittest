ed0530ff1e0ca113dda47d36dab11e944505687d
陈铭鹤 <615880048@qq.com>
2023-12-21 14:46:07 +0800

    111

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @author 陈铭鹤
+ * @version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello jindian!");
+    }
+}

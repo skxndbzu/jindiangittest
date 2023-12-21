d07fb1b2e01a5d8d796901398c941af172723188
陈铭鹤 <615880048@qq.com>
2023-12-21 15:58:17 +0800

    test2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello jindian!");
         System.out.println("test2再次进行修改");
+        System.out.println("develop进行修改");
     }
 }
